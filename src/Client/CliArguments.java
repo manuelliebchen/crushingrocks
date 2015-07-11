3dec16d250f1985f1072e4489fa25dd5869120fc
Maximilian Klockmann <maximilian.web@gmx.net>
2015-07-11 20:14:42 +0200

    Removed Optionals in CliArguments Add UnitTest for CliArguments
    Reviewd: Andreas

--- src/Client/CliArguments.java
+++ src/Client/CliArguments.java
@@ -10,12 +10,15 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
 /**
+ * Class for command-line parsing
  * @author Max Klockmann (max@acagamics.de)
- *
  */
 public class CliArguments {
 	private static final Logger LOG = Logger.getLogger(CliArguments.class);
 	
+	/**
+	 * automatic Parameter parsing.
+	 */
 	@Parameter(names = "--debug", description = "Debug mode")
 	private boolean debug = false;
 	@Parameter(names = "--help", description = "Show usage")
@@ -24,11 +27,12 @@ public class CliArguments {
 	@Parameter(names = "--nogui", description = "Don't show Gui")
 	private boolean noGui = false;
 	@Parameter(names = "--count", description = "Number of Games")
-	private int count = 1;
+	private int count = 0;
 	@Parameter(names = "--mode", description = "Selected gamemode")
 	private String mode = "";
-	@Parameter(names = "-bot", description = "Selected bot")
+	@Parameter(names = "--bot", description = "Selected bot")
 	private List<String> bots = new ArrayList<>();
+	
 	
 	public boolean isDebug() {
 		return this.debug;
@@ -50,13 +54,20 @@ public class CliArguments {
 		return this.bots;
 	}
 	
+	/**
+	 * Creating new CliArguments
+	 * @param args the arguments, which have to been parsed
+	 */
 	public CliArguments(String... args) {
+		// parse arguments
 		JCommander commander = new JCommander(this, args);
 		
+		// output parameters and descriptions
 		if (this.help) {
 			commander.usage();
 		}
 		
+		// enable debug mode
 		if (this.debug) {
 			LOG.setLevel(Level.DEBUG);
 		}
---
+++ test/Client/TestCliArguments.java
@@ -0,0 +1,90 @@
+package Client;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.beust.jcommander.ParameterException;
+
+public class TestCliArguments {
+
+	@Test
+	public void testCliArguments() {
+		CliArguments cli = new CliArguments();
+		
+		assertFalse(cli.isDebug());
+		assertTrue(cli.showGui());
+		assertEquals(0, cli.numberOfGames());
+		assertEquals("", cli.selectedGameMode());
+		assertEquals(new ArrayList<>(), cli.selectedBots());
+	}
+	
+	@Test
+	public void testCliArgumentsDebug() {
+		CliArguments cli = new CliArguments("--debug");
+		
+		assertTrue(cli.isDebug());
+		assertTrue(cli.showGui());
+		assertEquals(0, cli.numberOfGames());
+		assertEquals("", cli.selectedGameMode());
+		assertEquals(new ArrayList<>(), cli.selectedBots());
+	}
+	
+	@Test
+	public void testCliArgumentsNoGui() {
+		CliArguments cli = new CliArguments("--nogui");
+		
+		assertFalse(cli.isDebug());
+		assertFalse(cli.showGui());
+		assertEquals(0, cli.numberOfGames());
+		assertEquals("", cli.selectedGameMode());
+		assertEquals(new ArrayList<>(), cli.selectedBots());
+	}
+	
+	@Test
+	public void testCliArgumentsNumberOfGames() {
+		CliArguments cli = new CliArguments("--count 8".split(" "));
+		
+		assertFalse(cli.isDebug());
+		assertTrue(cli.showGui());
+		assertEquals(8, cli.numberOfGames());
+		assertEquals("", cli.selectedGameMode());
+		assertEquals(new ArrayList<>(), cli.selectedBots());
+	}
+	
+	@Test
+	public void testCliArgumentsGameMode() {
+		CliArguments cli = new CliArguments("--mode Test".split(" "));
+		
+		assertFalse(cli.isDebug());
+		assertTrue(cli.showGui());
+		assertEquals(0, cli.numberOfGames());
+		assertEquals("Test", cli.selectedGameMode());
+		assertEquals(new ArrayList<>(), cli.selectedBots());
+	}
+	
+	@Test
+	public void testCliArgumentsBots() {
+		CliArguments cli = new CliArguments("--bot Hallo.class --bot Welt.class".split(" "));
+		
+		assertFalse(cli.isDebug());
+		assertTrue(cli.showGui());
+		assertEquals(0, cli.numberOfGames());
+		assertEquals("", cli.selectedGameMode());
+		assertEquals(2, cli.selectedBots().size());
+		assertEquals("Hallo.class", cli.selectedBots().get(0));
+		assertEquals("Welt.class", cli.selectedBots().get(1));
+	}
+	
+	@Test(expected = ParameterException.class)
+	public void testCliArgumentsUnknwonParam() {
+		new CliArguments("--test");
+	}
+	
+	@Test(expected = ParameterException.class)
+	public void testCliArgumentsWrongNumber() {
+		new CliArguments("--count acht".split(" "));
+	}
+}
