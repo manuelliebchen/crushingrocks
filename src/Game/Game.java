f8c68a7f5bdcfaec2d58c5804fa8a2388e9c427d
Andreas Reich (@LAPTOP) <r_andreas2@web.de>
2015-07-12 18:02:35 +0200

    Added some getter for Game. Game needs now a list of player controller. Review: Claudius

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,7 +1,9 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import Game.Controller.IPlayerController;
 import Game.Logic.Coin;
 import Game.Logic.Map;
 import Game.Logic.Player;
@@ -9,18 +11,60 @@ import Game.Logic.Player;
 /**
  * MainClass for the GameSide. EntrancePoint for communication with ClientSide.
  * @author Jan-Cord Gerken (jancord@acagamics.de)
+ * @author Andreas Reich (andreas@acagamics.de)
  * 
  */
 public class Game {
-	
-	Map map;
-	ArrayList<Player> players;
+	private Map map;
+	private Player[] players;
 
-	/**When a player moves out of the map, he gets penaltyPoints*/
+	/** When a player moves out of the map, he gets penaltyPoints. */
 	final float OUT_OF_WORLD_PENALTY = -100F;
 	
-	public Game(){
-		players = new ArrayList<Player>();
+	/**
+	 * Creates a new map with the given player controller and a map.
+	 * @param playerController A list of player controller.
+	 */
+	public Game(List<IPlayerController> playerController){
+		assert(playerController != null);
+
+		players = new Player[playerController.size()];
+		for(int i=0; i<playerController.size(); ++i) {
+			assert(playerController.get(i) != null);
+			players[i] = new Player(playerController.get(i));
+		}
+		
+		map = new Map();
+	}
+	
+	/**
+	 * Retrieves game map.
+	 * @return A map. Surprise.
+	 */
+	public Map getMap() {
+		return map;
+	}
+	
+	/**
+	 * Returns a player with a given index.
+	 * @param index Index of the player to be returned. Needs to be bigger than zero.
+	 * @return Player or null if the index is out of range
+	 */
+	public Player getPlayer(int index) {
+		assert(index >= 0);
+		if(index < players.length)
+			return players[index];
+		else
+			return null;
+	}
+	
+	/**
+	 * Returns the number of players.
+	 * Note that the players may already be dead.
+	 * @return Player count.
+	 */
+	public int getNumPlayers(int index) {
+		return players.length;
 	}
 	
 	/**
