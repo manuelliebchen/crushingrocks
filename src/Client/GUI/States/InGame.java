a4c681c7ffb78e87ef0b76183f4e4ff8680d26be
Manuel Liebchen <hallo@manuelliebchen.de>
2019-11-10 22:24:33 +0100

    updating textures

--- src/Client/GUI/States/InGame.java
+++ src/Client/GUI/States/InGame.java
@@ -10,7 +10,6 @@ import Client.Rendering.Rendering.MapRendering;
 import Constants.DesignConstants;
 import Game.Controller.IPlayerController;
 import Game.Controller.PlayerControllerLoader;
-import Game.Controller.BuiltIn.HumanBot;
 import Game.Controller.BuiltIn.SampleBot;
 import Game.Logic.Game;
 import Game.Logic.GameStatistic;
@@ -40,7 +39,8 @@ public final class InGame extends GameState implements IDrawable, IUpdateable {
         playerLoader.loadControllerFromDirectory("Game/Controller/BuiltIn/");
 
         ArrayList<IPlayerController> playerControllers = new ArrayList<>();
-        playerControllers.add(playerLoader.instantiateLoadedExternController(HumanBot.class.getName()));
+//        playerControllers.add(playerLoader.instantiateLoadedExternController(HumanBot.class.getName()));
+        playerControllers.add(playerLoader.instantiateInternController(SampleBot.class.getName()));
         playerControllers.add(playerLoader.instantiateInternController(SampleBot.class.getName()));
 
         game = new Game(playerControllers);
