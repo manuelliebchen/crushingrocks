c51309cf3990d2b4eb9aa20db516f6f0a5258c1d
manuel.liebchen <manuel.liebchen@ovgu.de>
2019-12-13 13:07:46 +0100

    Version number 1.2

--- src/de/acagamics/constants/ClientConstants.java
+++ src/de/acagamics/constants/ClientConstants.java
@@ -11,7 +11,7 @@ public final class ClientConstants {
 	 * <p>
 	 * Value = {@value #VERSION}
 	 */
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.0";
 	/**
 	 * The url to the list of news.
 	 * <p>
